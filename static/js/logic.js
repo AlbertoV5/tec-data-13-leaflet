1178e92e9bd86b8a1a8ccb490acf4e86c3153132
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2022-09-29 16:48:19 -0500

    separated js files for better organization

--- static/js/logic.js
+++ static/js/logic.js
@@ -33,30 +33,6 @@ let map = L.map('mapid', {
 });
 L.control.layers(baseMaps, overlays).addTo(map);
 // JSON
-function getRadius(magnitude) {
-    if (magnitude === 0) {
-      return 1;
-    }
-    return magnitude * 4;
-}
-function getColor(magnitude){
-    if (magnitude > 5) {
-        return "#ea2c2c";
-    }
-    else if (magnitude > 4) {
-        return "#ea822c";
-    }
-    else if (magnitude > 3) {
-        return "#ee9c00";
-    }
-    else if (magnitude > 2) {
-        return "#eecc00";
-    }
-    else if (magnitude > 1) {
-        return "#d4ee00";
-    }
-        return "#98ee00";
-}
 function styleInfo(feature) {
     return {
       opacity: 1,
